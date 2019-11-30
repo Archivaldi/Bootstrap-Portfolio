2ba8d3e5ed64e721f01df1c4e7279f95f66dbef4
Artur Markov <artur.markov1860@gmail.com>
2019-11-30 11:30:38 -0800

    express and templates

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req,res) => {
+    res.render("index");
+});
+
+app.get("/portfolio", (req,res) => {
+    res.render("portfolio");
+});
+
+app.get("/contact", (req,res) => {
+    res.render("contact");
+});
+
+app.listen(PORT, function(){
+    console.log("listening on port " + PORT);
+});
